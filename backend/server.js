d452770906bd65fa53f02c5b8fd57449eaabf332
Vedant Aryan <vedantar2006@gmail.com>
2026-06-16 13:12:04 +0530

    chore: make port dynamic for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors({ origin: "*" }));
 
